1918972d471fe63ab1327a559d872ff7b844f488
dyCodes <yusufdaudu51@gmail.com>
2023-12-30 15:32:32 +0100

    feat: add axios config

---
+++ config/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+// Create a custom Axios instance
+const apiClient = axios.create({
+  baseURL: "https://share-safe-85lb.onrender.com",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Set the Bearer token in the headers
+// apiClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+// Remove the Bearer token in the headers
+// delete axios.defaults.headers.common["Authorization"];
+
+export { apiClient };
